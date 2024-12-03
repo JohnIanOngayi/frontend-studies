fde3b1728d03e4026af87eef4ad2c6bb380c26b3
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-03 12:22:58 +0300

    events and event-listeners

--- js-dom-manipulation/js/dom.js
+++ js-dom-manipulation/js/dom.js
@@ -1,35 +1,19 @@
-// Examine the DOM
-// GETELEMENTBYID
-// var headerTitle = document.getElementById("header-title");
-// console.log(headerTitle);
-// headerTitle.textContent = "Hello";
-// headerTitle.innerText = "World!";
-// headerTitle.innerHTML = "<h3>Hello</h3>";
-// headerTitle.style.borderBottom = "2px solid red";
-// var items = document.getElementsByClassName("list-group-item");
-// items[1].style.backgroundColor = "yellow";
-// for (var i = 0; i < items.length; i++) {
-//   items[i].style.color = "blue";
+// function buttonClick(event) {
+// console.log(event.target.id);
+// console.log(event.target.classList);
+// alert("button clicked!");
+//
+// var output = document.getElementById("output");
+// output.innerHTML = "<h3>" + event.target.id + "</h3>";
+//
+// console.log(event.type);
+// console.log(event.clientX, event.clientY);
+// console.log(event.offsetX, event.offsetY);
+//   console.log(event.altKey);
+//   console.log(event.ctrlKey);
+//   console.log(event.altKey);
 // }
-
-// var li = document.getElementsByTagName("li");
-// console.log(li);
-
-var li = document.querySelector("li");
-// console.log(li.parentNode);
-// console.log("nextSibling", li.nextSibling);
-// console.log("nextElementSibling", li.nextElementSibling);
-var ul = document.querySelector("ul");
-// console.log(ul.children);
-// console.log(ul.firstElementChild);
-var newDiv = document.createElement("div");
-newDiv.className = "hello bob";
-newDiv.id = "hello-1";
-newDiv.setAttribute("title", "new");
-var newDivText = document.createTextNode("Hello World");
-newDiv.appendChild(newDivText);
-var container = document.querySelector("header .container");
-var h1 = document.querySelector("header h1");
-container.insertBefore(newDiv, h1);
-newDiv.style.fontSize = "25px";
-console.log(newDiv);
+//
+// var button = document
+//   .getElementById("button")
+//   .addEventListener("click", buttonClick);
